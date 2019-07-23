985072a443ba765087179940b3d64a93f4c3f373
arnaudkelbert <arnaud.kelbert@gmail.com>
2019-07-23 18:10:31 +0200

    qt4/5 support

--- pyQArk/Widgets/QArkMplPlotWidget/__qarkqt4__QArk3DDataMplPlotter.py
+++ pyQArk/Widgets/QArkMplPlotWidget/__qarkqt4__QArk3DDataMplPlotter.py
@@ -6,9 +6,33 @@
 #
 #
 # @author : Arnaud Kelbert
-# @date : 2018/03/30
-# @version : 0.1
-#-----------------------------------------------------------------------
+# @date : 2019/07/23
+# @version : 0.2
+#
+# Historic:
+# 0.1 : init version
+# 0.2 : add python 2/3 compatibility
+# -----------------------------------------------------------------------
+# {-- Pyhton 2/3 compatibility ------------------------------------------
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+import sys
+try:
+    from future import standard_library
+    standard_library.install_aliases()
+
+    from builtins import (ascii, bytes, chr, dict, filter, hex, input,
+                          int, map, next, oct, open, pow, range, round,
+                          str, super, zip)
+except ImportError:
+    if sys.version_info.major == 2:
+        print('Warning : future package is missing - compatibility issues between python 2 and 3 may occur')
+try:
+    # Python 2 : basestring exists (for isinstance test)
+    basestring
+except:
+    # Python 3 : basestring does not exist
+    basestring = str
+# }-- Pyhton 2/3 compatibility ------------------------------------------
 from PyQt4 import QtCore
 
 import matplotlib
@@ -16,7 +40,7 @@ matplotlib.use('QT4Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-from .QArkMplPlotter import QArkMplPlotter
+from pyQArk.Widgets.QArkMplPlotWidget.QArkMplPlotter import QArkMplPlotter
 
 class QArk3DDataMplPlotter( QArkMplPlotter ):
 
