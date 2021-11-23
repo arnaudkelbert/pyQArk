912ec731c3c7f41b32f48551b9931196588d43bc
arnaud kelbert <arnaud.kelbert@gmail.com>
2021-11-23 22:27:31 +0100

    avoid an exception with python 2 : change to not call 'exec' function from object

--- pyQArk/Core/QArkQt.py
+++ pyQArk/Core/QArkQt.py
@@ -35,6 +35,6 @@ except:
 
 def _exec(obj,*args,**kwargs):
     if hasattr(obj, 'exec'):
-        return obj.exec(*args, **kwargs)
+        return getattr(obj, 'exec')(*args, **kwargs)
     elif hasattr(obj, 'exec_'):
         return obj.exec_(*args, **kwargs)
