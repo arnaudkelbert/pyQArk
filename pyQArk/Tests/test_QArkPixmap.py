7bfcd424315b5547c7dd2cb5c8823269f3a5e74d
arnaud kelbert <arnaud.kelbert@gmail.com>
2022-01-25 18:34:04 +0100

    Commit

--- pyQArk/Tests/test_QArkPixmap.py
+++ pyQArk/Tests/test_QArkPixmap.py
@@ -68,7 +68,11 @@ class QArkPixmapTest(unittest.TestCase):
             self.o_layout.addWidget(self.o_widget)
 
             #Test QPixmap directly
-            o_image2 = QtGui.QImage('./pixmap_grey.png')
+            import os
+            from pyQArk import QArkConfig
+            s_img = os.path.join(QArkConfig.QARK_ROOT_DIR, 'Tests', 'pixmap.png')
+            #o_image2 = QtGui.QImage('./pixmap_grey.png')
+            o_image2 = QtGui.QImage(s_img)
             #o_image2 = QtGui.QImage('./pixmap.jpg')
             o_pixmap2 = QtGui.QPixmap.fromImage(o_image2)
             self.o_widget2 = QtWidgets.QLabel()
